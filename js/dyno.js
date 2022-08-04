aa989c898b2056591b03a7c5ee4eae6bb1f84f8d
brunopontsz <brunopontsz@gmail.com>
2022-08-04 14:35:52 -0300

    introduced navbar

--- js/dyno.js
+++ js/dyno.js
@@ -20,3 +20,13 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+var nav = document.querySelector('nav');
+
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('bg-dark', 'shadow');
+        } else {
+          nav.classList.remove('bg-dark', 'shadow');
+        }
+      });
