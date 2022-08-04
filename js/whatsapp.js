9e2ea1ec84930d01ec8519c82a27646bf762bcf6
brunopontsz <brunopontsz@gmail.com>
2022-08-04 17:44:12 -0300

    ocurrencies at cta area

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -3,11 +3,13 @@ function gotowhatsapp() {
     var name = document.getElementById("contact-name").value;
     var vehicle = document.getElementById("contact-vehicle").value;
     var motor = document.getElementById("contact-motor").value;
+    var message = document.getElementById("contact-message").value;
 
     var url = "https://wa.me/5585997360914?text=" 
-    + "Olá, me chamo " + name + ", gostaria de saber mais sobre os serviços de potência que a Dynotest tem a oferecer para meu veículo."
-    + "%0A" + "Veículo:"+ vehicle + "%0A" 
-    + "Motorização: " + motor; 
+    + "Olá DynoTest, tudo bem?" + "%0A" +"Me chamo " + "*"+ name + "*" + " e gostaria de saber mais sobre os serviços que vocês oferecem para meu veículo."
+    + "%0A" + "Veículo: "+ "*" + vehicle + "*" + "%0A" 
+    + "Motorização: " + "*"+ motor + "*" +"%0A" 
+    + "Mensagem: " + "*"+ message + "*"; 
 
     window.open(url, '_blank').focus();
 }
