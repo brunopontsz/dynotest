a65f6c9554c5b9a7abeeae483f35b49c8dabc37a
brunopontsz <brunopontsz@gmail.com>
2022-08-05 11:52:18 -0300

    finalized cta modal

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -3,13 +3,13 @@ function gotowhatsapp() {
     var name = document.getElementById("contact-name").value;
     var vehicle = document.getElementById("contact-vehicle").value;
     var motor = document.getElementById("contact-motor").value;
-    var message = document.getElementById("contact-message").value;
+    var service = document.getElementById("contact-service").value;
 
     var url = "https://wa.me/5585997360914?text=" 
     + "Olá DynoTest, tudo bem?" + "%0A" +"Me chamo " + "*"+ name + "*" + " e gostaria de saber mais sobre os serviços que vocês oferecem para meu veículo."
     + "%0A" + "Veículo: "+ "*" + vehicle + "*" + "%0A" 
     + "Motorização: " + "*"+ motor + "*" +"%0A" 
-    + "Mensagem: " + "*"+ message + "*"; 
+    + "Serviço: " + "*"+ service + "*"; 
 
     window.open(url, '_blank').focus();
 }
