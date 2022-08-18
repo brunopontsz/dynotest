ac09d6251bf1fd72b24c9394a0c1f17c83e4997d
Marly Martins <marlymartins@MacBook-Pro-de-Marly.local>
2022-08-18 14:50:47 -0300

    removed smooth scroll for test

--- js/dyno.js
+++ js/dyno.js
@@ -1,18 +1,5 @@
 
-$('.navbar a').on('click', function(e) {
-    if (this.hash !== '') {
-        e.preventDefault();
 
-        const hash = this.hash;
-
-        $('html, body').animate(
-        {
-            scrollTop: $(hash).offset().top
-        },
-        800
-        );
-    }
-});
 
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
