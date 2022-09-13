c20bc88c233951373d9f1b60c2a6a159ab643625
Bruno Pontes <brunopontsz@gmail.com>
2022-09-12 22:06:57 -0300

    fix: improve code

--- js/dyno.js
+++ js/dyno.js
@@ -1,21 +1,15 @@
 
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+$(document).ready(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
 
-var nav = document.querySelector('nav');
-
-      window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 100) {
-          nav.classList.add('bg-dark', 'shadow');
-        } else {
-          nav.classList.remove('bg-dark', 'shadow');
-        }
-      });
-
-      $(document).ready(function(){
-        $('[data-bs-toggle="tooltip"]').tooltip();   
-    });
+  var nav = document.querySelector('nav');
+  
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+      nav.classList.add('bg-dark', 'shadow');
+    } else {
+      nav.classList.remove('bg-dark', 'shadow');
+    }
+  });
+});
--- js/whatsapp.js
+++ js/whatsapp.js
@@ -12,42 +12,31 @@ function openModal() {
 }
 
 function goToWhatsapp () {
-    let selected = [];
-    for (var option of document.getElementById('contact-service').options)
-    {
-        if (option.selected) {
-            selected.push(option.value);
-        }
-    }
-    
+    const breakLine = `%0A`;
+
+    const selectedOptions = [...document.getElementById('contact-service').options].filter(t => t.selected);
     const name = document.getElementById("contact-name").value;
     const vehicle = document.getElementById("contact-vehicle").value;
-    let motor = document.getElementById("contact-motor").value;
-    const breakLine = `%0A`;
+    const engine = document.getElementById("contact-motor").value;
+    
+    //Input Validation
+    if (!name || !vehicle) {
+        return false;
+    }
+
+
+    //requiredFields
     const text = `Olá DynoTest, tudo bem? ${breakLine}Me chamo *${name}* e gostaria de saber mais sobre os serviços que vocês oferecem para meu veículo. ${breakLine}`;
-    const vehicleText = `${breakLine}*Veículo: ${vehicle}*`;
+    const vehicleText = `${breakLine}Veículo:${breakLine}*${vehicle}*:${breakLine}`;
 
-    let engineText;
-    if (motor == "") {
-        engineText = ``;
-    } else {
-        engineText = `${breakLine}*Motorização: ${motor}*`;
-    }
-
-    let serviceText;
-    if (selected == "") {
-        serviceText = ``;
-    } else {
-        serviceText = `${breakLine}*Servico: ${selected}*`;
-    }
+    //optional fields
+    const engineText = !!engine ? `${breakLine}Motorização:${breakLine}*${engine}*:${breakLine}` : '';
+    const serviceText = !!selectedOptions ? 
+        `${breakLine}Serviço(s) escolhido(s): ${selectedOptions.map(t => `${breakLine}- *${t.value}*`)}` : '';
   
-
-
     const finalText = text + vehicleText + engineText + serviceText;
-    const urlPath = `https://wa.me/5585997360914?text=${finalText}`;
+    const urlPath = `https://wa.me/5585982190975?text=${finalText}`;
     window.open(urlPath, '_blank').focus();
-
-
-}    
+}
 
 
